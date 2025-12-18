5fb9231d2ff94a9a95d0005ff4ee73fda141995b
Anh Le Xuan <anh.lexuan05@gmail.com>
2025-12-18 19:08:22 +0100

    Add set_seed function for reproducibility in experiments

---
+++ 06_projects/utils.py
@@ -0,0 +1,11 @@
+
+def set_seed(seed=42):
+    import torch, random, numpy as np, os
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed)
